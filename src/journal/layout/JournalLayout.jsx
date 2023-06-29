300290378ccbbb2db54da3c02911cfcd24087d88
ezemg <ezequielmatiasgr@gmail.com>
2023-06-29 13:50:48 -0300

    Maquetado Listo

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,17 +1,19 @@
-import { Box } from '@mui/material';
+import { Box, Toolbar } from '@mui/material';
 import React from 'react';
+import { NavBar, SideBar } from '../components';
 
 const drawerWidth = 240;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex' }}>
-      {/* {Navbar} */}
-      {/* {SideBar} */}
+      <NavBar drawerWidth={drawerWidth} />
+      <SideBar drawerWidth={drawerWidth} />
 
       <Box
         component="main"
         sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
         {children}
       </Box>
     </Box>
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,16 +1,34 @@
-import { Typography } from '@mui/material';
+import { IconButton, Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout.jsx';
+import { NothingSelectedView, NoteView } from '../views';
+import { AddOutlined } from '@mui/icons-material';
 
 export const JournalPage = () => {
   return (
     <>
       <JournalLayout>
-        <Typography>
+        {/* <Typography>
           Nulla officia incididunt aute magna occaecat Lorem, ipsum dolor sit
           amet consectetur adipisicing elit. Dolores quibusdam soluta illo dicta
           quisquam enim accusantium tempore provident in sed nulla, perferendis,
           sequi ab earum ex exercitationem. Omnis, natus atque.
-        </Typography>
+        </Typography> */}
+
+        {/* <NothingSelectedView /> */}
+        <NoteView />
+
+        <IconButton
+          size="large"
+          sx={{
+            color: 'white',
+            backgroundColor: 'error.main',
+            ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+            position: 'fixed',
+            right: 50,
+            bottom: 50,
+          }}>
+          <AddOutlined sx={{ fontSize: 30 }} />
+        </IconButton>
       </JournalLayout>
     </>
   );
